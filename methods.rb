67e6d7a714654f43f169ce22f606e086cd2baf10
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-10 10:04:19 -0800

    commit tree 9eac4a00c0bab9d84b1cce5e1219a0876d65d013

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+  end 
+
+  def greet (name="Naureen")
+    puts "Hello, #{name}!"
+  end
+greet
+
+def greet (name="Jimmy")
+  puts "Hello, #{name}!"
+end
+greet
+
+def greet_with_default (name="programmer")
+  puts "Hello, #{name}!"
+end
+greet ("Naureen")
+
+def add (num1,num2)
+  num1+num2
+end
+add(2,5)
+
+def halve (number)
+  if number.class != number
+    return nil
+  end
+  halve 6/2
+end
